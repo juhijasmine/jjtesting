88dc747fb2b8e0a9aebeed2d97ce00908620fc7f
Katacoda Scenario <scenario@katacoda.com>
2021-03-04 09:47:56 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function isNullorEmpty(input){
-	return (input===undefined || input ==== null || input ===='');
-}
-module.exports=isNullorEmpty;
+function isNullOrEmpty(input) {
+	// Returns true if the input is either undefined, null, or empty, false otherwise
+	 return (input === undefined || input === null || input === '');
+ }
+ // Export to make the function available to other packages
+ module.exports = isNullOrEmpty;
